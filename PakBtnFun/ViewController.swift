75fcd984b086da2971ee98f388b4f180fd6589cf
dit05 <dit@dit05ui-imac.local>
2019-03-21 00:50:38 -0700

    2019-03-21

--- PakBtnFun/ViewController.swift
+++ PakBtnFun/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Labelc: UILabel!
+    @IBOutlet weak var LabelB: UILabel!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //myLabel.text = "lololol"
+        Labelc.text = String(count)
+        
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        
+        count = count + 1
+        myLabel.text = "버튼 A을 누르셨습니다."
+        Labelc.text = String(count)
+    }
+    
+    @IBAction func ButtonBPressed(_ sender: Any) {
+        LabelB.text = "버튼 B을 누르셨습니다."
+    }
 }
 
